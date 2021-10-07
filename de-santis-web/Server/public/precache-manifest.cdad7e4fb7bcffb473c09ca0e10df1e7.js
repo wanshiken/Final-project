cb57060e82b766714a9f7d977f58d0d6db5fbad8
Juan De Santis <juanbds66@gmail.com>
2021-10-07 11:54:01 +0200

    deploy

---
+++ de-santis-web/Server/public/precache-manifest.cdad7e4fb7bcffb473c09ca0e10df1e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fe65133bd459775d9d8",
+    "url": "/static/css/main.9348af65.chunk.css"
+  },
+  {
+    "revision": "6fe65133bd459775d9d8",
+    "url": "/static/js/main.ef2e063f.chunk.js"
+  },
+  {
+    "revision": "b8cfdcb03b94e3659859",
+    "url": "/static/js/runtime~main.a11142f1.js"
+  },
+  {
+    "revision": "6d0525be8ef3585a8b9f",
+    "url": "/static/css/2.25969b07.chunk.css"
+  },
+  {
+    "revision": "6d0525be8ef3585a8b9f",
+    "url": "/static/js/2.06730bbf.chunk.js"
+  },
+  {
+    "revision": "914da6b62f7cee96ff6a",
+    "url": "/static/js/3.a317e0df.chunk.js"
+  },
+  {
+    "revision": "17cc444cc79d1f2eaa2a4aa5b85187e2",
+    "url": "/static/media/Logo de santis beats png.17cc444c.png"
+  },
+  {
+    "revision": "e20f1e8608bdbf4b1c5df661dab9fbd8",
+    "url": "/static/media/Rauw Alejandro - Cosa Guapa.e20f1e86.mp3"
+  },
+  {
+    "revision": "67a390395150ff0348ce2522beb7e820",
+    "url": "/index.html"
+  }
+];
